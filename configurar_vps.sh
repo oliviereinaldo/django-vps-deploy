4d583bd6126f046131644863094be3525d4ba97d
Olivier Cristian <129457641+oliviereinaldo@users.noreply.github.com>
2026-02-09 21:58:52 -0300

    Update configurar_vps.sh
    Signed-off-by: Olivier Cristian <129457641+oliviereinaldo@users.noreply.github.com>

--- configurar_vps.sh
+++ configurar_vps.sh
@@ -366,7 +366,7 @@ LOGGING = {
         "app_debug": {
             "level": "DEBUG",
             "class": "logging.FileHandler",
-            "filename": os.path.join(LOG_PATH, "app-debug.log"),
+            "filename": os.path.join(LOG_PATH, "app_debug.log"),
             "formatter": "verbose",
         },
         "console": {
@@ -394,11 +394,16 @@ EOL
 # ================================
 # CRIA STATIC E LOGS
 # ================================
+
 mkdir -p "$SITE_DIR/staticfiles"
+
 sudo mkdir -p "$DJANGO_LOG_PATH"
+sudo chown www-data:www-data "$DJANGO_LOG_PATH"
+sudo chmod 2775 "$DJANGO_LOG_PATH"
+
 sudo touch "$DJANGO_LOG_PATH/error.log" "$DJANGO_LOG_PATH/app_debug.log"
 sudo chown www-data:www-data "$DJANGO_LOG_PATH"/*.log
-sudo chmod 640 "$DJANGO_LOG_PATH"/*.log
+sudo chmod 664 "$DJANGO_LOG_PATH"/*.log
 
 # ================================
 # INSTALA NGINX SE NÃO EXISTIR
